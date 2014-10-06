a098ac22ec57533afbfd89b7c172706f31f3eebc
Dhruv13 <dhruvthakkar9@gmail.com>
2014-10-06 13:30:26 +0530

    updated

---
+++ CallByRefAndOut/CallByRefAndOut/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Test
+{
+    static void check(out int a,int b)
+    {
+        a = 100;
+        b = 200;
+
+    }
+    static void Main()
+    {
+        int a = 1, b = 2;
+        check(out a,b);
+        Console.WriteLine(a + "   " + b);
+
+    }
+}
